ce8e176944af6488d3de0c2d65d19b988231ba0e
Elizaveta Korchevaya <lisa@Elizavetas-MacBook-Pro.local>
2024-03-31 23:50:30 +0200

    bug fixes

--- Voronoi.py
+++ Voronoi.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.spatial import Voronoi
-from Bio.PDB import PDBParser, NeighborSearch, calc_surface_area
+from Bio.PDB import PDBParser, NeighborSearch
 import sys
 
 def pdb_file_prep(pdb_file):
@@ -16,21 +16,21 @@ def pdb_file_prep(pdb_file):
                     residues.append(residue['CA'].get_coord())
     return structure, residues
 
-def get_accesibility(pdb_file):
-    """Calculates the solvent accessibility of each residue in a protein structure by calculating solvent-accessible surface area."""
+"""def get_accesibility(pdb_file):
+    #Calculates the solvent accessibility of each residue in a protein structure by calculating solvent-accessible surface area.
     structure, residues = pdb_file_prep(pdb_file)
     neighbors = NeighborSearch(residues)
     accessibility = calc_surface_area(structure, neighbors)
-    return accessibility
+    return accessibility"""
 
 def get_voronoi(pdb_file):
     """Creates a Voronoi diagram from the alpha carbon atoms of the entire protein structure."""
-    residues = pdb_file_prep(pdb_file)
+    structure, residues = pdb_file_prep(pdb_file)
     voronoi = Voronoi(residues)
     return voronoi
 
 def get_voronoi_volumes(voronoi):
-    """Calculates the volume of each Voronoi cell."""
+    #Calculates the volume of each Voronoi cell.
     volumes = []
     for i, region in enumerate(voronoi.regions):
         if not -1 in region:
@@ -38,17 +38,17 @@ def get_voronoi_volumes(voronoi):
             volumes.append(voronoi.volume(indices))
     return volumes
 
-def detect_cavities(voronoi, pdb_file):
-    """Detects the cavities in the protein structure based on the Voronoi cell volumes and accessibility."""
+"""def detect_cavities(voronoi, pdb_file):
+    #Detects the cavities in the protein structure based on the Voronoi cell volumes and accessibility.
     volumes = get_voronoi_volumes(voronoi)
     accessibility = get_accesibility(pdb_file)
     cavities = []
     # Thresholds for cavity detection: one for volume one for accessibility
     # Integration of both metrics
     # Rank the cavities based on the integrated metric
-    return cavities
+    return cavities"""
 
-"""def access_voronoi_data(voronoi):
+def access_voronoi_data(voronoi):
     p = voronoi.points
     v = voronoi.vertices
     r = voronoi.regions
@@ -65,4 +65,4 @@ if __name__ == '__main__':
           f"voronoi regions: {voronoi.regions}\n"
           f"voronoi ridge points: {voronoi.ridge_points}\n"
           f"voronoi ridge vertices: {voronoi.ridge_vertices}\n"
-          f"voronoi point region: {voronoi.point_region}")"""
+          f"voronoi point region: {voronoi.point_region}")
